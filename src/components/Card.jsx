630367af43d7b197d3b019604d6b1c8abb7741c0
rohit <raj0eshsing0909@gmail.com>
2023-10-26 00:37:30 +0530

    ui added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Hosp from "../images/hosp.jpg"
 
 export default function Card() {
   return (
-    <div>
+    <>
       <section>
         <div className="flex flex-col items-center justify-center mb-[5px]">
           <h1 className="text-[70px] font-bold xs:text-[30px] text-slate-600 py-[10px]">
@@ -16,20 +16,20 @@ export default function Card() {
           </h4>
         </div>
         <div className=" items-center py-[60px]">
-          <div className=" grid   justify-center  xs:flex-col md:flex sm:gap-[30px] lg:items-center xl:items-center gap-[75px] 2xl:items-center w-full space-x-6 ">
-            <div className="  shadow-md hover:scale-105 transition-transform duration-700 ease-in-out transform sm:w-[350px] justify-center">
-              <div className="shadow-xl rounded-xl h-[400px] flex flex-col items-center justify-center">
+          <div className=" grid justify-center  xs:flex-col md:flex sm:gap-[30px] lg:items-center xl:items-center gap-[75px] 2xl:items-center w-full space-x-6 justify-items-center">
+            <div className=" hover:scale-105 transition-transform duration-700 ease-in-out transform sm:w-[320px]">
+              <div className="shadow-xl rounded-xl h-[400px] flex flex-col items-center border border-red-200 w-[300px] sm:w-[350px] md:w-[350px] lg:w-[350px]">
                 <div className=" rounded-xl shadow-gray-70 flex justify-center">
                   <img
                     src={Renu}
                     alt="dr.renu"
                     height="160px"
-                    width="300px"
+                    width="240px"
                     className="rounded-xl shadow-xl h-[287px]"
                   />
                 </div>
                 <div className="w-[300px] mb-[5px] text-center ">
-                  <p className="py-[40px]">
+                  <p className="py-[20px]">
                     <p className="font-semibold text-xl text-slate-600">
                       Dr. Renu Garkoti
                     </p>
@@ -42,19 +42,19 @@ export default function Card() {
               </div>
             </div>
 
-            <div className="shadow-md hover:scale-105 transition-transform duration-700 ease-in-out transform sm:w-[350px] ">
-              <div className="shadow-xl rounded-xl h-[400px] flex flex-col items-center justify-center">
+            <div className=" hover:scale-105 transition-transform duration-700 ease-in-out transform sm:w-[340px] ">
+              <div className="shadow-xl rounded-xl h-[400px] flex flex-col items-center justify-center border border-red-200 w-[300px] sm:w-[350px] md:w-[350px] lg:w-[350px]">
                 <div className="rounded-xl flex justify-center">
                   <img
                     src={Hosp}
                     alt="hospital "
                     height="300px"
-                    width="300px"
-                    className="rounded-xl shadow-xl h-[287px]"
+                    width="250px"
+                    className="rounded-xl shadow-xl h-[287px] sm:w-[280px]"
                   />
                 </div>
-                <div className="w-[300px]  text-center px-[10px]">
-                  <p className="py-[40px]">
+                <div className="w-[300px] mt-[20px] text-center px-[10px]">
+                  <p className="py-[10px]">
                     <p className=" font-semibold text-2xl text-slate-600">
                       RJ Hospital
                     </p>
@@ -67,19 +67,19 @@ export default function Card() {
               </div>
             </div>
 
-            <div className="shadow-md hover:scale-105 transition-transform duration-700 ease-in-out transform  w-[400px]">
-              <div className="shadow-xl rounded-xl h-[400px] w-auto flex flex-col items-center justify-center">
+            <div className="hover:scale-105 transition-transform duration-700 ease-in-out transform sm:w-[350px]">
+              <div className="shadow-xl rounded-xl h-[400px] flex flex-col items-center justify-center w-[300px] border border-red-200 sm:w-[350px] md:w-[350px] lg:w-[350px]">
                 <div className=" rounded-xl flex items-center justify-center">
                   <img
                     src={Jagdish}
                     alt="dr.jagdish"
                     height="150px"
-                    width="auto"
-                    className="rounded-xl h-[287px]"
+                    width="240px"
+                    className="rounded-xl h-[287px] sm:w-[270px]"
                   />
                 </div>
                 <div className="w-[300px] mb-[5px] text-center">
-                  <p className="py-[35px]">
+                  <p className="py-[20px]">
                     <p className="font-semibold text-2xl text-slate-600">
                       Dr. Jagdish Garkoti
                     </p>
@@ -93,6 +93,6 @@ export default function Card() {
           </div>
         </div>
       </section>
-    </div>
+    </>
   );
 }
