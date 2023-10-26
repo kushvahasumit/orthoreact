9ef50d1e93de18bf645d57cba80bb7fe44776e71
rohit <raj0eshsing0909@gmail.com>
2023-10-26 21:22:25 +0530

    apppointment form added

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -106,7 +106,7 @@ export default function Appointment() {
           <div class="px-[10px] w-[600px] shadow-xl py-[30px] rounded-lg">
             <div className="px-[10px]">
               <span class="uppercase text-sm text-gray-600 font-bold">
-                Full Name
+                Full Name:
               </span>
               <input
                 class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
@@ -115,20 +115,68 @@ export default function Appointment() {
               />
             </div>
             <div class="mt-8 px-[10px]">
+              <span class="uppercase text-sm text-gray-600 font-bold">Age</span>
+              <input
+                class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="number"
+              />
+            </div>
+            {/* <div class="mt-8 px-[10px]">
               <span class="uppercase text-sm text-gray-600 font-bold">
-                Email
+                Message
+              </span>
+              <textarea class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+            </div> */}
+            <div class="mt-8 px-[10px]">
+              <span class="uppercase text-sm text-gray-600 font-bold">
+                Phone:
+              </span>
+              <input
+                class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                type="Number"
+              />
+            </div>
+
+            <div class="mt-8 px-[10px]">
+              <span class="uppercase text-sm text-gray-600 font-bold">
+                Address:
               </span>
               <input
                 class="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
                 type="text"
               />
             </div>
+
             <div class="mt-8 px-[10px]">
               <span class="uppercase text-sm text-gray-600 font-bold">
-                Message
+                Gender:
               </span>
-              <textarea class="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+              <input
+                type="radio"
+                name="male"
+                value="male"
+                id="gender"
+                className="mx-2"
+              />
+              Male
+              <input
+                type="radio"
+                name="female"
+                value="female"
+                id="gender"
+                className="mx-2"
+              />
+              Female
+              <input
+                type="radio"
+                name="other"
+                value="other"
+                id="gender"
+                className="mx-2"
+              />
+              Other
             </div>
+
             <div className="mt-8 ml-[-50px] px-[10px]">
               <button className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-[200px] focus:outline-none focus:shadow-outline hover:bg-indigo-300">
                 Submit
